9b56fa954a1205664845e886502c09601e97a2f0
Caranille <mycaranille@gmail.com>
2014-08-23 18:33:34 +0200

    Début de la vérification du compte

--- Serveur/Module1.vb
+++ Serveur/Module1.vb
@@ -28,6 +28,19 @@ Module Main
 
     Sub Login(ByVal Pseudo As String, ByRef Password As String)
         'Lancement de la connection sur le serveur SQL
+        Dim myAdapter As New MySqlDataAdapter
+        Dim sqlquery = "SELECT * FROM cnx_user Where login = '" + Pseudo + "' AND password '" + Password
+        Dim myCommand As New MySqlCommand()
+        myCommand.Connection = conn
+        myCommand.CommandText = sqlquery
+        myAdapter.SelectCommand = myCommand
+        Dim myData As MySqlDataReader
+        myData = myCommand.ExecuteReader
+        If myData.HasRows = 0 Then
+            Console.WriteLine("Pseudo ou mot de passe incorrect")
+        Else
+            Console.WriteLine("Identification acceptée")
+        End If
     End Sub
 
 End Module
