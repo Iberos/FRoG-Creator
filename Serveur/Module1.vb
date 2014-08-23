2d914ffa981cbf6189cf3c43ba072c47b087f22c
Caranille <mycaranille@gmail.com>
2014-08-23 18:19:00 +0200

    Début du serveur avec Mysql

---
+++ Serveur/Module1.vb
@@ -0,0 +1,35 @@
+﻿Imports MySql.Data.MySqlClient
+
+Module Main
+
+    Dim server As String
+    Dim user As String
+    Dim password As String
+    Dim database As String
+    Dim conn As New MySqlConnection
+
+    Sub Main()
+
+    End Sub
+
+    Sub CheckBDD(ByVal server As String, ByRef user As String, ByRef password As String, ByRef database As String)
+        conn.ConnectionString = "server=" + server + ";" _
+        & "user id=" + user + ";" _
+        & "password=" + password + ";" _
+        & "database=" + database + ";"
+        Try
+            conn.Open()
+
+        Catch ex As MySqlException
+            ' Si il y a un problème
+            Console.WriteLine("Connection à la base de donnée MYSQL impossible")
+        End Try
+    End Sub
+
+    Sub Login(ByVal Pseudo As String, ByVal Password As String)
+        'Lancement de la connection sur le serveur SQL
+        Dim connStr As String = "DRIVER={MySQL ODBC 5.1 Driver};SERVER=server;UID=user;DATABASE=database;Password=password"
+        Dim Connection As New System.Data.Odbc.OdbcConnection(connStr)
+    End Sub
+
+End Module
