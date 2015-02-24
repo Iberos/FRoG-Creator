54b3255702fbc01c6bb28f7e848a05349759b4a3
Iberos <9fgj0c4c@randomail.net>
2015-02-24 19:19:38 +0100

    Terminé NetworkBuffer côté serveur

--- Serveur/Network/NetworkBuffer.vb
+++ Serveur/Network/NetworkBuffer.vb
@@ -1,15 +1,27 @@
-﻿Public Class NetworkBuffer
+﻿Imports System.Text
+
+Public Class NetworkBuffer
 
     Private Sub WriteByte(ByVal index As Byte, ByVal value As Byte)
-
+        tmpClient(index).stream.WriteByte(value)
     End Sub
 
     Private Sub WriteInteger(ByVal index As Byte, ByVal value As Integer)
-
+        Dim tmp() As Byte = ASCIIEncoding.UTF8.GetBytes(value.ToString())
+        tmpClient(index).stream.WriteByte(CByte(value.ToString().Length))
+        For i = 0 To value.ToString().Length - 1
+            tmpClient(index).stream.WriteByte(tmp(i))
+        Next
     End Sub
 
     Private Sub WriteString(ByVal index As Byte, ByVal value As String)
-
+        If (value.Length < 256) Then
+            Dim tmp() As Byte = ASCIIEncoding.UTF8.GetBytes(value)
+            tmpClient(index).stream.WriteByte(CByte(value.Length))
+            For i = 0 To value.ToString().Length - 1
+                tmpClient(index).stream.WriteByte(tmp(i))
+            Next
+        End If
     End Sub
 
     Public Sub Write(ByVal index As Byte, ByVal value As Object)
@@ -23,19 +35,22 @@
         End Select
     End Sub
 
-    Public Function ReadByte(ByVal index As Byte) As Byte
-
-        Return 0
+    Private Function ReadByte(ByVal index As Byte) As Byte
+        Return tmpClient(index).stream.ReadByte()
     End Function
 
-    Public Function ReadInteger(ByVal index As Byte) As Integer
-
-        Return 0
+    Private Function ReadInteger(ByVal index As Byte) As Integer
+        Dim length As Byte = tmpClient(index).stream.ReadByte()
+        Dim tmp(length) As Byte
+        tmpClient(index).stream.Read(tmp, 0, length)
+        Return Convert.ToInt32(ASCIIEncoding.UTF8.GetString(tmp))
     End Function
 
-    Public Function ReadString(ByVal index As Byte) As String
-
-        Return 0
+    Private Function ReadString(ByVal index As Byte) As String
+        Dim length As Byte = tmpClient(index).stream.ReadByte()
+        Dim tmp(length) As Byte
+        tmpClient(index).stream.Read(tmp, 0, length)
+        Return ASCIIEncoding.UTF8.GetString(tmp)
     End Function
 
     Public Function Read(ByVal index As Byte, ByVal type As TypeCode) As Object
